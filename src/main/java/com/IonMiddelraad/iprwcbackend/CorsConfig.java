c0dae317378d46ac20cc6a4661349d8da5da57c5
BoopidyBappidy <83213010+IonMiddelraad@users.noreply.github.com>
2023-03-19 20:01:39 +0100

    added deployment settings

--- src/main/java/com/IonMiddelraad/iprwcbackend/CorsConfig.java
+++ src/main/java/com/IonMiddelraad/iprwcbackend/CorsConfig.java
@@ -6,6 +6,20 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+//@Configuration
+//public class CorsConfig {
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurerAdapter() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("*")
+//                        .allowedHeaders("*");
+//            }
+//        };
+//    }
+//}
+
 @Configuration
 public class CorsConfig {
     @Bean
@@ -13,7 +27,7 @@ public class CorsConfig {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("*")
+                registry.addMapping("/*").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("")
                         .allowedHeaders("*");
             }
         };
